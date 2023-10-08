e12b2a4fdcc37298e1764b14c470150d62b2af78
Ajmal2025 <139298518+Ajmal2025@users.noreply.github.com>
2023-10-08 21:47:08 +0500

    Add files via upload

---
+++ assignment Q2/index.js
@@ -0,0 +1,12 @@
+"use strict";
+//q3
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase,
+// uppercase, and titlecase.
+let n = "ErIc";
+console.log(`Hello ${n.toLowerCase()}, would you like to learn some Python today?`);
+console.log(`Hello ${n.toUpperCase()}, would you like to learn some Python today?`);
+const titleCase = n
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+console.log(`Hello ${titleCase}, would you like to learn some Python today?`);
